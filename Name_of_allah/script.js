1afbd288d0e13c410ba9b714c00f9c9973ec4132
Yasmine Khemir <yasminekhemir471@gmail.com>
2026-04-15 20:48:01 +0200

    premier commit

---
+++ Name_of_allah/script.js
@@ -0,0 +1,6 @@
+function task(){
+    
+    document.createElement("p");
+    document.getElement("p").value = document.getElementById("UserText").value
+    
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+function addTask(){
+    document.querySelector('submit');
+    let task = document.createElement("p")
+    task.innerHTML =  "<p>La vie est belle</p>"
+}

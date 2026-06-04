6274b5424eaf55de133529f7f05ec8f18ad4b5c1
Yasmine Khemir <yasminekhemir471@gmail.com>
2026-06-04 20:09:19 +0200

    Fix: validation input vide et correction appendChild

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function addTask(){
     let inputValue = document.getElementById('writeTask').value;
-    let task = document.createElement("input");
-    task.type="checkbox";
+    
 
     if(inputValue === ""){
         return;
     }
+    
+    let task = document.createElement("input");
+    task.type="checkbox";
     // task.textContent =  inputValue; don't use for chekbox
     let labelText = document.createElement("label");
     labelText.textContent = inputValue
     
-
-    document.querySelector('#TasksPlace').appendChild(task);
-    document.querySelector('#TasksPlace').appendChild(labelText)
-
     // create one element for checkbox and value 
     let containerTasks = document.createElement('div');
     containerTasks.appendChild(task);
